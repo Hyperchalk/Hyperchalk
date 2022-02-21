4fbda398b2957e47a517da5bcc8d66345abe2b5e
Lukas Menzel <lukas@menzel.earth>
2022-02-21 16:12:32 +0100

    runserver messages

--- devscripts/runserver.sh
+++ devscripts/runserver.sh
@@ -4,5 +4,8 @@ cd $BASEDIR/..
 
 $BASEDIR/make_static.sh
 
+echo $(colored $CYAN "migrating database... ")
 gosu moodle ENV/bin/python manage.py migrate --settings=local_settings
+
+echo $(colored $CYAN "starting development server:")
 gosu moodle ENV/bin/python manage.py runserver --settings=local_settings
