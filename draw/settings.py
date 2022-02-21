ec4c9fd22b4771035d9893fab3c85a804a10c95a
Lukas Menzel <lukas@menzel.earth>
2022-02-21 14:46:48 +0100

    fix migration

--- draw/settings.py
+++ draw/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Settings for LTI ToolConfig
 # see lti.tool_config.py
