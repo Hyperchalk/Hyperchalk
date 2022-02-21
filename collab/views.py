cbcce069780c56336cd07ad168da6f9d769853ee
Lukas Menzel <lukas@menzel.earth>
2022-02-21 13:49:27 +0100

    fix initial user name loading

--- collab/views.py
+++ collab/views.py
@@ -1,8 +1,8 @@
-from asgiref.sync import sync_to_async
 from channels.db import database_sync_to_async
 from django.conf import settings
 from django.http import HttpRequest, JsonResponse
 from django.shortcuts import redirect, render
+from django.utils.translation import gettext_lazy as _
 
 from draw.utils import reverse_with_query
 
@@ -11,27 +11,29 @@ from .utils import make_room_name
 
 get_room_elements = database_sync_to_async(m.ExcalidrawRoom.objects.get_or_create)
 
+@database_sync_to_async
+def get_username(user):
+    return (user.first_name or user.username) if user.pk else _("Anonymous")
+
 async def index(request: HttpRequest, **kwargs):
+    """
+    The index view will generate a new room if
+    """
     room = request.GET.get('room', None)
     if not room:
         room_uri = reverse_with_query('collab:index', query_kwargs={'room': make_room_name(24)})
         return redirect(request.build_absolute_uri(room_uri), permanent=False)
 
     room_obj, _ = await get_room_elements(room_name=room)
-    username = 'wolobums'
-    user = await sync_to_async(request.user) # TODO: getting user information does not work here...
-    if user.id:
-        username = user.first_name or user.username
     return render(request, 'collab/index.html', {'excalidraw_config': {
         'SOCKET_URL': request.build_absolute_uri('/ws/collab/collaborate/' + room)\
             .replace('http://', 'ws://', 1),
-        'BROADCAST_RESOLUTION': 50,
+        'BROADCAST_RESOLUTION': 100,
         'ROOM_NAME': room,
         'INITIAL_DATA': room_obj.elements,
-        'USER_NAME': username,
+        'USER_NAME': await get_username(request.user),
         'LANGUAGE_CODE': settings.LANGUAGE_CODE,
         'ELEMENT_UPDATES_BEFORE_FULL_RESYNC': 100
-        # TODO: USER_COLOR
     }})
 
 async def get_current_elements(request: HttpRequest, room_name: str):
