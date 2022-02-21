a64393cc5f1dc08e2637b784266122f6ad6f38bf
Lukas Menzel <lukas@menzel.earth>
2022-02-21 14:59:07 +0100

    env setup script

---
+++ devscripts/setup_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+BASEDIR=$(cd "$(dirname "$0")"; pwd)
+cd $BASEDIR/..
+
+rm -rf ENV
+python -m venv ENV
+ENV/bin/python -m pip install -U pip wheel setuptools
+ENV/bin/python -m pip install -r requirements.txt
