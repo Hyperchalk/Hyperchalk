a434d992fefe9f149c3e532f34d7d254d90b3e01
Lukas Menzel <lukas@menzel.earth>
2022-02-21 16:02:45 +0100

    minor fixes

--- devscripts/setup_env.sh
+++ devscripts/setup_env.sh
@@ -3,6 +3,6 @@ BASEDIR=$(cd "$(dirname "$0")"; pwd)
 cd $BASEDIR/..
 
 rm -rf ENV
-python -m venv ENV
+python3 -m venv ENV
 ENV/bin/python -m pip install -U pip wheel setuptools
 ENV/bin/python -m pip install -r requirements.txt
