37bed388d902bbde0e7de5c6c3043be827b65ed4
Lukas Menzel <lukas@menzel.earth>
2022-02-21 16:07:26 +0100

    update runserver script

--- devscripts/runserver.sh
+++ devscripts/runserver.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+BASEDIR=$(cd "$(dirname "$0")"; pwd)
+cd $BASEDIR/..
+
+$BASEDIR/make_static.sh
+
+gosu moodle ENV/bin/python manage.py migrate --settings=local_settings
 gosu moodle ENV/bin/python manage.py runserver --settings=local_settings
