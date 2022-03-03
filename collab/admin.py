fb4f643e863c7130ba0cdd7b4deb492829435071
Lukas Menzel <lukas@menzel.earth>
2022-03-03 17:19:25 +0100

    add replay mode to admin

--- collab/admin.py
+++ collab/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import reverse
-from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
 from . import models as m
@@ -27,17 +27,31 @@ class ExcalidrawLogRecordAdmin(admin.ModelAdmin):
             json_link = reverse('collab:record-json', kwargs={
                 'room_name': obj.room_name, 'pk': obj.pk
             })
-            return mark_safe(f"<a href={json_link}>Go to JSON</a>")
+            return format_html(
+                "<a href={json_link}>{text}</a>",
+                json_link=json_link, text=_("Go to JSON"))
         return _('will be generated after saving')
 
 
 @admin.register(m.ExcalidrawRoom)
 class ExcalidrawRoomAdmin(admin.ModelAdmin):
-    readonly_fields = ['room_json']
+    readonly_fields = ['room_json', 'replay_link']
 
-    @admin.display(description="View Room in Browser JSON Viewer")
+    @admin.display(description=_("View Room in Browser JSON Viewer"))
     def room_json(self, obj: m.ExcalidrawRoom):
         if obj.pk:
             room_link = reverse('collab:room-json', kwargs={'room_name': obj.room_name})
-            return mark_safe(f"<a href='{room_link}'>Go to room JSON</a>")
+            return format_html(
+                "<a href='{room_link}'>{text}</a>",
+                room_link=room_link,
+                text=_('Go to room JSON'))
+        return _('will be generated after saving')
+
+    @admin.display(description=_("Replay Mode"))
+    def replay_link(self, obj: m.ExcalidrawRoom):
+        if obj.pk:
+            room_link = reverse('collab:replay-room', kwargs={'room_name': obj.room_name})
+            return format_html(
+                "<a href='{room_link}'>{text}</a>",
+                room_link=room_link, text=_("Replay this room"))
         return _('will be generated after saving')
