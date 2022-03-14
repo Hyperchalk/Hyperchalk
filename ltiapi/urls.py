3d313509c8767ee0dd4dfdd7a315182468fd2f78
Lukas Menzel <lukas@menzel.earth>
2022-03-14 18:51:41 +0100

    implement assignment modes: classroom, group, single

--- ltiapi/urls.py
+++ ltiapi/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('login', views.oidc_login, name="login"),
     path('jwks', views.oidc_jwks, name="jwks"),
     path('launch', views.lti_launch, name="launch"),
+    path('configure/<launch_id>', views.lti_configure, name="configure"),
     # path('config', views.lti_config, name="config"),
 ]
