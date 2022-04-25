1bad023e544c640268229acf16dd2fc8165e85db
Lukas Menzel <lukas@menzel.earth>
2022-04-25 16:03:42 +0200

    fix privacy view url

--- ltiapi/urls.py
+++ ltiapi/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path(
         'register-consumer/<uuid:pk>', views.RegisterConsumerView.as_view(),
         name="register-consumer"),
-    path('privacy', TemplateView.as_view(template='ltiapi/privacy.html')),
+    path('privacy', TemplateView.as_view(template_name='ltiapi/privacy.html')),
     path('login', views.oidc_login, name="login"),
     path('jwks', views.oidc_jwks, name="jwks"),
     path('launch', views.lti_launch, name="launch"),
