dc5d0e2279ff4e7084b550b3ff892f529c3508a9
Lukas Menzel <lukas@menzel.earth>
2022-09-22 16:00:10 +0200

    allow configuration via env vars

--- draw/env_settings.py
+++ draw/env_settings.py
@@ -1,56 +1,53 @@
 """
-Local settings can be defined here. For local development, this file can simply be copied
-to ``local_settings.py``. However, here is a _WARNING_: this file SHOULD be adapted for
-production uses. Otherwise there _WILL_ be security issues within the app.
+Local settings can be defined here.
 
 The settings are explained in https://docs.djangoproject.com/en/3.2/topics/settings/
 
 You should also have a look at https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 """
 # pylint: disable=wildcard-import,unused-wildcard-import
+from os import environ as env
+
+import dj_database_url
+
 from draw.settings import *
 from draw.utils import deepmerge
 
-# TODO: important persons to the project. Those who get the mails when critical events happen.
+# important persons to the project. Those who get the mails when critical events happen.
 
-# ADMINS = [('John Doe', 'doe@example.com')]
-# MANAGERS = ADMINS
+ADMINS = [(env.get("HC_ADMIN_NAME"), env.get("HC_ADMIN_MAIL"))]
+MANAGERS = ADMINS
 
 # allow the creation of rooms when the user visits the index page
 # without specifying a room in the URL. (default is false.)
-# ALLOW_AUTOMATIC_ROOM_CREATION = True
+ALLOW_AUTOMATIC_ROOM_CREATION = env.get("HC_ALLOW_AUTOMATIC_ROOM_CREATION") == "true"
 
 # If set to false (default), users will need to be logged in.
-# ALLOW_ANONYMOUS_VISITS = True
+ALLOW_ANONYMOUS_VISITS = env.get("HC_ALLOW_ANONYMOUS_VISITS") == "true"
 
 # These rooms are accessible without any authentication or authorization. List of room names.
-# PUBLIC_ROOMS = []
-
-# TODO: uncomment this if you are going to use SQLite. Otherwise you can delete it.
-# DATA_DIR = BASE_DIR / 'data'
+PUBLIC_ROOMS = env.get("HC_PUBLIC_ROOMS", "").split(",")
 
 # Should NEVER be true in production! Set to True for debug messages if you encounter an error.
-DEBUG = False
+DEBUG = env.get("HC_DEBUG") == "true"
 
-# TODO: Change this and uncomment! You can get a good key by executing the following command:
+# You can get a good key by executing the following command:
 # < /dev/urandom tr -dc 'A-Za-z0-9!#$%&()*+,-./:;<=>?@[\]^_`{|}~' | head -c64; echo
 # the result won't include the quote-characters so you can safely put the output string in quotes
 
-# SECRET_KEY = "super_secret_key_that_has_to_be_changed_in_production!!!"
+SECRET_KEY = env.get("HC_SECRET_KEY")
+if not SECRET_KEY:
+    raise KeyError("No HC_SECRET_KEY provided")
 
-# TODO: add your host name(s) here!
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = env.get("HC_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 # When you register an LTI consumer, you can call the command './manage.py makeconsumerlink'.
 # This command will display a URL to you that can be used to automatically configure this app
 # via the LTI Advantage registration protocol in consumers that support it. To be able to do
 # this, you have to configure your host name here, which will be used to generate said link.
 # The LINK_BASE will also be added to the list of CSRF_TRUSTED_ORIGINS as "https://LINK_BASE".
-#
-# TODO: uncomment this and enter your host name.
 
-# LINK_BASE = 'localhost:8000'
+LINK_BASE = env.get("HC_LINK_BASE")
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -60,25 +57,12 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # how to configure this to integrate with your database.
 #
 # WARNING: do not use sqlite with more than one thread / process!
-#
-# TODO: uncommment this and enter your database settings here.
 
-# DATABASES = {
-#     # uncomment this if you want SQLite
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME':   DATA_DIR / 'db.sqlite3',
-#     },
-#     # uncomment this if you want MySQL / Postgres
-#     'default': {
-#         'ENGINE':   'django.db.backends.mysql',       # uncomment for mysql
-#         'ENGINE':   'django.db.backends.postgresql',  # uncomment for postgres
-#         'HOST':     'TODO', # db host. name of your docker service if you connect a db via docker.
-#         'NAME':     'TODO', # name of your database
-#         'USER':     'TODO',
-#         'PASSWORD': 'TODO',
-#     },
-# }
+# NOTE: the environment variable where this gets its config from is called "DATABASE_URL"
+DATA_DIR = BASE_DIR / 'data'
+DATABASES = {
+    'default': dj_database_url.config(default=f'sqlite:///{DATA_DIR / "db.sqlite3"}'),
+}
 
 # Caching
 # https://docs.djangoproject.com/en/4.0/topics/cache/
@@ -87,19 +71,19 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # LTI nonce over multiple requests on tool configuration, the cache needs to be the same for all
 # worker processes. This cannot be achieved via the in-memory caching-mechanism. If you use redis
 # anyway as the channels backend, you may want to just use it for caching, too.
-#
-# TODO: uncomment to configure caching. required if you want to run more than one worker process.
 
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-#         # 6379 is the default redis port. if you use docker, the hostname is the name of
-#         # the redis service. if you have a redis cluster, just add all servers to the list,
-#         # beginning with the leader. the url scheme is: 'redis://username:password@host:port'
-#         # or 'redis://host:port' if authentication is disabled on your redis.
-#         'LOCATION': ['redis://redis:6379']
-#     }
-# }
+DEFAULT_REDIS = "redis://redis:6379"
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        # 6379 is the default redis port. if you use docker, the hostname is the name of
+        # the redis service. if you have a redis cluster, just add all servers to the list,
+        # beginning with the leader. the url scheme is: 'redis://username:password@host:port'
+        # or 'redis://host:port' if authentication is disabled on your redis.
+        'LOCATION': env.get("HC_REDIS_URL", DEFAULT_REDIS).split(",")
+    }
+}
 
 # Channel Layers
 # https://channels.readthedocs.io/en/latest/topics/channel_layers.html
@@ -118,29 +102,18 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # [1]: https://channels.readthedocs.io/en/stable/topics/channel_layers.html#redis-channel-layer
 # [2]: https://pypi.org/project/channels-redis/
 # [3]: https://github.com/danidee10/channels_postgres/
-#
-# TODO: uncomment this and configure your desired channel layer
 
-# CHANNEL_LAYERS = {
-#     # uncomment this if you only run one worker process
-#     "default": {
-#         "BACKEND": "channels.layers.InMemoryChannelLayer",
-#     },
-#     # uncomment this if you have more than one worker process and use redis
-#     "default": {
-#         "BACKEND": "channels_redis.core.RedisChannelLayer",
-#         "CONFIG": {
-#             # schema of this tuple is (hostname, port). if you use
-#             # docker, the hostname is the name of your service.
-#             "hosts": [("redis", 6379)],
-#         },
-#     },
-# }
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": env.get("HC_REDIS_URL", DEFAULT_REDIS).split(","),
+        },
+    },
+}
 
 # Logging
 # https://docs.djangoproject.com/en/3.2/topics/logging/
-#
-# TODO: if you need some custom logging, you can configre it here.
 
 LOGGING = deepmerge(LOGGING, {
     'handlers': {
@@ -165,33 +138,21 @@ LOGGING = deepmerge(LOGGING, {
 #
 # Though this application does not send custom mails (yet), you might be interested in configuring
 # mail, so get admin-mailed when critical events get logged.
-#
-# TODO: configure mail
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = ...
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = ...
-# EMAIL_HOST_PASSWORD = ...
-# EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = False                     # mutually exclusive to EMAIL_USE_TLS
-# EMAIL_SUBJECT_PREFIX = '[Hyperchalk]'
-
-# Static files
-# https://docs.djangoproject.com/en/3.2/ref/contrib/staticfiles/
-#
-# usually, you don't need to change this.
-#
-# TODO: if you want to serve some custom static files, configure them here.
-
-# STATICFILES_DIRS.append(BASE_DIR / 'tmp')
+if env.get("HC_EMAIL_HOST"):
+    EMAIL_BACKEND = env.get("HC_EMAIL_BACKEND", 'django.core.mail.backends.smtp.EmailBackend')
+    EMAIL_HOST = env.get("HC_EMAIL_HOST")
+    EMAIL_PORT = int(env.get("HC_EMAIL_PORT", "465"))
+    EMAIL_HOST_USER = env.get("HC_EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = env.get("HC_EMAIL_HOST_PASSWORD")
+    EMAIL_USE_TLS = env.get("HC_EMAIL_USE_TLS", "true") == "true"
+    EMAIL_USE_SSL = env.get("HC_EMAIL_USE_SSL", "false") == "true" # mutually exclusive to EMAIL_USE_TLS
+    EMAIL_SUBJECT_PREFIX = env.get("HC_EMAIL_SUBJECT_PREFIX", '[Hyperchalk]')
 
 # Time Zones
 # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-#
-# TODO: configure your time zone.
 
-TIME_ZONE = 'CET'
+TIME_ZONE = env.get("HC_TIME_ZONE", 'CET')
 
 # NOTE: IMPORTANT. DO NOT REMOVE.
 finalize_settings(locals())
